6beab80aad589fab7f29689ef52bba7bd7e1fc5e
Francesco <firefelix@gmail.com>
2015-08-21 18:06:35 +0200

    computing size after every update

--- src/InputLink.js
+++ src/InputLink.js
@@ -64,8 +64,8 @@ const InputLink = React.createClass({
     );
   },
 
-  componentWillReceiveProps() {
-    setTimeout(this.computeChildSize);
+  componentDidUpdate() {
+    this.computeChildSize();
   }
 
 });
