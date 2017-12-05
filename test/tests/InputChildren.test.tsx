ff506dcfa9ab57cac51d86ce8e099768dff283c6
Francesco Cioria <firefelix@gmail.com>
2017-12-05 17:22:28 +0100

    add test for build script

--- test/tests/InputChildren.test.tsx
+++ test/tests/InputChildren.test.tsx
@@ -1,3 +1,6 @@
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
 import * as React from 'react';
 import { render } from 'enzyme';
 import InputChildren from '../../src';
@@ -26,3 +29,12 @@ it('should render correctly', () => {
 //   });
 
 });
+
+describe('build', () => {
+
+  it('build script generates every needed file', () => {
+    execSync('npm run build')
+    expect(fs.readdirSync(path.resolve(__dirname, '../../lib'))).toMatchSnapshot()
+  })
+
+})
